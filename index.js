aa332d69e36937a444153994edd8fecc3de7c96f
WJP2003 <31232449+WJP2003@users.noreply.github.com>
2018-10-23 20:30:18 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,16 +4,12 @@ var http = require('http');
 var port = process.env.PORT || 8080;
 
 http.createServer(function (req, res) {
-	function respond {
-		res.writeHead(200, {'Content-Type': 'text/plain'});
-		res.write("Hello World!");
-		res.end(); // end the response
-	}
-	
 	if(req.url != "/") {
 		res.writeHead(302, {'Location': '/'});
 		res.write("Redirecting to <a href=\"/\">here</a>");
 	} else {
-		respond();
+		res.writeHead(200, {'Content-Type': 'text/plain'});
+		res.write("Hello World!");
+		res.end(); // end the response
 	}
 }).listen(port);
