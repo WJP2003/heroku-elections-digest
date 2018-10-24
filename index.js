7c6055ad853a84dbb26968c32da128a8a382711e
WJP2003 <31232449+WJP2003@users.noreply.github.com>
2018-10-23 20:23:32 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var port = process.env.PORT || 8080;
 http.createServer(function (req, res) {
 	if(req.url != "/") {
 		res.writeHead(302, {'Location': '/'});
-	});
+	}
 
 	res.writeHead(200, {'Content-Type': 'text/plain'});
 	res.write("Hello World!");
