62b2b50232b0bff325e55e7a7cb86a6bf6bee7a8
WJP2003 <31232449+WJP2003@users.noreply.github.com>
2018-10-23 19:55:35 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 // index.js
 
-var http = require('http');
+var http = require('http'
+var port = process.env.PORT || 8080;
 
 http.createServer(function (req, res) {
 	res.writeHead(200, {'Content-Type': 'text/plain'});
 	res.write('Hello World!'); // write a response to the client
 	res.end(); // end the response
-}).listen(8080);
+}).listen(port);
