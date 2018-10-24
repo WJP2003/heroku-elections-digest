8dfb835d351d91d6f4db8745f2c671dd36f313a6
WJP2003 <31232449+WJP2003@users.noreply.github.com>
2018-10-23 20:57:38 -0500

    Update index.js

--- index.js
+++ index.js
@@ -10,9 +10,11 @@ http.createServer(function (req, res) {
 		res.write("Redirecting to <a href=\"/\">here</a>");
 	} else {
 		fs.readFile('house_of_reps.svg', function(err, data) {
-			res.writeHead(200, {'Content-Type': 'text/html'});
-			res.write(data);
-			res.end();
+			fs.readFile('jsbundle.html',function(err2,data2) {	
+				res.writeHead(200, {'Content-Type': 'text/html'});
+				res.write(data2 + data);
+				res.end();
+			});
 		});
 	}
 }).listen(port);
