c9770c970ccd705a688f8acedddfe7fc07cdc6f5
WJP2003 <31232449+WJP2003@users.noreply.github.com>
2018-10-24 21:02:12 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,62 @@
 // index.js
 
 var http = require('http');
+var https = require('https');
 var fs = require('fs');
 var port = process.env.PORT || 8080;
+var districts = {
+	'AL': 7,
+	'AK': 0,
+	'AZ': 9,
+	'AR': 4,
+	'CA': 55,
+	'CO': 7,
+	'CN': 5,
+	'DE': 0,
+	'FL': 27,
+	'GA': 14,
+	'HI': 2,
+	'ID': 2,
+	'IL': 18,
+	'IN': 13,
+	'IA': 4,
+	'KS': 4,
+	'KY': 6,
+	'LA': 6,
+	'ME': 2,
+	'MD': 8,
+	'MA': 9,
+	'MI': 14,
+	'MN': 8,
+	'MS': 4,
+	'MO': 8,
+	'MT': 0,
+	'NE': 3,
+	'NV': 4,
+	'NH': 2,
+	'NJ': 12,
+	'NM': 3,
+	'NY': 27,
+	'NC': 13,
+	'ND': 0,
+	'OH': 16,
+	'OK': 5,
+	'OR': 5,
+	'PN': 18,
+	'RI': 2,
+	'SC': 7,
+	'SD': 0,
+	'TN': 9,
+	'TX': 36,
+	'UT': 4,
+	'VT': 0,
+	'VA': 11,
+	'WA': 10,
+	'WV': 3,
+	'WI': 8,
+	'WY': 0
+}
+		
 
 http.createServer(function (req, res) {
 	if(req.url != "/") {
@@ -10,10 +64,63 @@ http.createServer(function (req, res) {
 		res.write("Redirecting to <a href=\"/\">here</a>");
 	} else {
 		fs.readFile('house_of_reps.svg', function(err, data) {
-			fs.readFile('jsbundle.html',function(err2,data2) {	
-				res.writeHead(200, {'Content-Type': 'text/html'});
-				res.write("<html><head>" + data2 + data);
-				res.end();
+			fs.readFile('jsbundle.html',function(err2,data2) {
+				https.get('https://www.cookpolitical.com/ratings/house-race-ratings', function(err3,resp) {
+					data3 = '';
+					resp.on('data',function(chunk) {
+						data3 += chunk;
+					});
+					resp.on('end',function() {
+						solidR = [];
+						likelyR = [];
+						leanR = [];
+						tossup = [];
+						leanD = [];
+						likelyD = [];
+						solidD = [];
+						
+						for(i = 1,i <= 55,i++) {
+							str2 = data3.slice(0,str.indexOf("CA-" + i));
+							str3 = str2.slice(0,str2.lastIndexOf("</p>"));
+							str4 = str3.slice(str3.lastIndexOf("solid-seats-modal-in-title"),str3.length);
+							str5 = str4.slice(str4.lastIndexOf(">")+1,str4.length);
+							if(str5 == "Solid Republican") {
+								solidR.push("CA-" + i);
+							} else {
+								if(str5 == "Likely Republican") {
+									likelyR.push("CA-" + i);
+								} else {
+									if(str5 == "Lean Republican") {
+										leanR.push("CA-" + i);
+									} else {
+										if(str5 == "Toss-Up Republican") {
+											tossup.push("CA-" + i);
+										} else {
+											if(str5 == "Toss-Up Democratic") {
+												tossup.push("CA-" + i);
+											} else {
+												if(str5 == "Lean Democratic") {
+													leanD.push("CA-" + i);
+												} else {
+													if(str5 == "Likely Democratic") {
+														likelyD.push("CA-" + i);
+													} else {
+														if(str5 == "Solid Democratic") {
+															solidD.push("CA-" + i);
+														}
+													}
+												}
+											}
+										}
+									}
+								}
+							}
+						}
+						res.writeHead(200, {'Content-Type': 'text/html'});
+						res.write("<html><head>" + solidR + "\n" + likelyR + "\n" + leanR + "\n" + tossup + "\n" + leanD + "\n" + likelyD + "\n" + solidD + "\n" + data2 + data);
+						res.end();
+					});
+				});
 			});
 		});
 	}
