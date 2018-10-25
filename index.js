b8628db2e9f3d54372426ee65443e75872ec1fbd
WJP2003 <31232449+WJP2003@users.noreply.github.com>
2018-10-24 22:02:18 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var https = require('https');
 var fs = require('fs');
 var port = process.env.PORT || 8080;
 
-var house_districts = [['AL', 7],['AK', 0],['AZ', 9],['AR', 4],['CA', 53],['CO', 7],['CN', 5],['DE', 0],['FL',27],['GA',14],['HI',2],['ID',2],['IL',18],['IN',13],['IA',4],['KS',4],['KY',6],['LA',6],['ME',2],['MD',8],['MA',9],['MI',14],['MN',8],['MS',4],['MO',8],['MT',0],['NE',3],['NV',4],['NH',2],['NJ',12],['NM',3],['NY',27],['NC',13],['ND',0],['OH',16],['OK',5],['OR',5],['PN',18],['RI',2],['SC',7],['SD',0],['TN',9],['TX',36],['UT',4],['VT',0],['VA',11],['WA',10],['WV',3],['WI',8],['WY',0]]
+var house_districts = [['AL', 7],['AK', 0],['AZ', 9],['AR', 4],['CA', 53],['CO', 7],['CT', 5],['DE', 0],['FL',27],['GA',14],['HI',2],['ID',2],['IL',18],['IN',9],['IA',4],['KS',4],['KY',6],['LA',6],['ME',2],['MD',8],['MA',9],['MI',14],['MN',8],['MS',4],['MO',8],['MT',0],['NE',3],['NV',4],['NH',2],['NJ',12],['NM',3],['NY',27],['NC',13],['ND',0],['OH',16],['OK',5],['OR',5],['PA',18],['RI',2],['SC',7],['SD',0],['TN',9],['TX',36],['UT',4],['VT',0],['VA',11],['WA',10],['WV',3],['WI',8],['WY',0]]
 
 http.createServer(function (req, res) {
 	if(req.url != "/") {
@@ -35,9 +35,9 @@ http.createServer(function (req, res) {
 								str3 = str2.slice(0,str2.lastIndexOf("</p>"));
 								str4 = str3.slice(str3.lastIndexOf("solid-seats-modal-in-title"),str3.length);
 								str5 = str4.slice(str4.lastIndexOf(">")+1,str4.length);
-								console.log(house_districts[j][0] + i + ": " + str5);
+								console.log(house_districts[j][0] + "-" + i + ": " + str5);
 								if(str5 == "Solid Republican") {
-									solidR.push(house_districts[j][0] + i);
+									solidR.push(house_districts[j][0] + "-" + i);
 								} else {
 									if(str5 == "Likely Republican") {
 										likelyR.push(house_districts[j][0] + "-" + i);
