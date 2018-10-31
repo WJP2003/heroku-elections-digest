2a5925ca6663bbe243aabde3fcc25cb3105d925e
WJP2003 <31232449+WJP2003@users.noreply.github.com>
2018-10-31 18:34:51 -0500

    Update index.js

--- index.js
+++ index.js
@@ -161,7 +161,7 @@ racesDownload = function() {
 									str4 = str3.slice(str3.lastIndexOf('<p class="ratings-detail-page-table-7-column-cell-title">'),str3.lastIndexOf('</p>'));
 								}
 								str5 = str4.replace('<p class="ratings-detail-page-table-7-column-cell-title">','');
-								console.log(house_districts[j][0] + "-" + i + " - " + str4);
+								console.log(house_districts[j][0] + "-" + i + " - " + str5);
 							} else {
 								console.log(house_districts[j][0] + "-" + i + " - N/A");
 							}
