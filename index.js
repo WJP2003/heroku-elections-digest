d2dbda1fe8f950c0d269812081de557260698c42
WJP2003 <31232449+WJP2003@users.noreply.github.com>
2018-10-31 13:00:20 -0500

    Update index.js

--- index.js
+++ index.js
@@ -156,6 +156,8 @@ racesDownload = function() {
 							if(str2 == data4.slice(0,data4.lastIndexOf(">" + house_districts[j][0] + "-"))) {
 								str2 = data4.split(">" + house_districts[j][0] + "-");
 								str2 = str2[str2.length - 1];
+							} else {
+								str2 = data4.slice(0,data4.lastIndexOf(">" + house_districts[j][0] + "-"));
 							}
 							str3 = str2.slice(str2.lastIndexOf('<div class="ratings-detail-page-table-7-column">'),str2.length);
 							str4 = str3.slice(str3.indexOf('<p class="ratings-detail-page-table-7-column-cell-title">'),str3.length);
