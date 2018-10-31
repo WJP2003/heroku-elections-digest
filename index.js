d44ff176cc617f8ba7bbf80001bfe6128c49183e
WJP2003 <31232449+WJP2003@users.noreply.github.com>
2018-10-31 00:31:27 -0500

    Update index.js

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ races_download = function() {
 				
 					for(j = 0;j < states.length;j++) {
 						for(i = 1;i <= 2;i++) {
-             						str2 = data4.slice(0,data4.lastIndexOf(">" + house_districts[j][0] + "-"));
+							str2 = data4.slice(0,data4.lastIndexOf(">" + house_districts[j][0] + "-"));
 							str3 = str2.slice(str2.lastIndexOf('<div class="ratings-detail-page-table-7-column">'),str2.length);
 							str4 = str3.slice(str3.indexOf('<p class="ratings-detail-page-table-7-column-cell-title">'),str3.length);
 							str5 = '';
@@ -111,12 +111,14 @@ races_download = function() {
 													if(str6 == "SOLID D") {
 														solidD.push(house_districts[j][0] + "-" + i);
 													}
-												}												}
-											}
+												}
+											}												}
 										}
 									}
 								}
 							}
+						}
+					}
 					
 					strSolidR = "['" + solidR.join("','") + "']" ;
 					strLikelyR = "['" + likelyR.join("','") + "']";
