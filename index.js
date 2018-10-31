287a025c51087fb35f0c24dc6536cc6f17e9311a
WJP2003 <31232449+WJP2003@users.noreply.github.com>
2018-10-31 13:25:44 -0500

    Update index.js

--- index.js
+++ index.js
@@ -155,13 +155,12 @@ racesDownload = function() {
 							console.log(house_districts[j][0] + "-" + i);
 							if(str2 == data4.slice(0,data4.lastIndexOf(">" + house_districts[j][0] + "-"))) {
 								str2 = data4.split(">" + house_districts[j][0] + "-");
-								str2 = str2[str2.length - 1];
+								str2 = str2[str2.length - 2];
 								console.log("IF A : " + str2);
 							} else {
 								str2 = data4.slice(0,data4.lastIndexOf(">" + house_districts[j][0] + "-"));
 								console.log("IF B : " + str2);
 							}
-							console.log("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
 							str3 = str2.slice(str2.lastIndexOf('<div class="ratings-detail-page-table-7-column">'),str2.length);
 							str4 = str3.slice(str3.indexOf('<p class="ratings-detail-page-table-7-column-cell-title">'),str3.length);
 							str5 = '';
