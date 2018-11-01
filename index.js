31a2461944afc8ed5e34ad2e781a19b55d614fea
WJP2003 <31232449+WJP2003@users.noreply.github.com>
2018-10-31 21:19:14 -0500

    Update index.js

--- index.js
+++ index.js
@@ -161,7 +161,7 @@ racesDownload = function() {
 								str3 = str2.slice(str2.lastIndexOf('<div class="ratings-detail-page-table-7-column">',str2.length));
 								str4 = ''
 								console.log("length: " + str3.split('<ul class="ratings-detail-page-table-7-column-ul">').length);
-								if(str3.split('<ul class="ratings-detail-page-table-7-column-ul">').length == 1) {
+								if(str3.split('<ul class="ratings-detail-page-table-7-column-ul">').length == 2) {
 									str4 = str3.slice(str3.indexOf('<p class="ratings-detail-page-table-7-column-cell-title">'),str3.indexOf('</p>'));
 								} else {
 									str4 = str3.slice(str3.lastIndexOf('<p class="ratings-detail-page-table-7-column-cell-title">'),str3.lastIndexOf('</p>'));
