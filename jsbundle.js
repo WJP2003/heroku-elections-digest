ba0dc41a45a2cc096780277ec1342f814bb59c85
WJP2003 <31232449+WJP2003@users.noreply.github.com>
2018-10-31 21:57:15 -0500

    Update jsbundle.js

--- jsbundle.js
+++ jsbundle.js
@@ -62,65 +62,93 @@ runColoration = function() {
 	
 	for(i = 0;i < houseSolidR.length;i++) {
 		console.log("Set as Solid R: " + houseSolidR[i]);
-		document.getElementById(houseSolidR[i]).setAttribute("class","Solid Rep");
+		if(document.getElementById(houseSolidR[i]) != undefined) {
+			document.getElementById(houseSolidR[i]).setAttribute("class","Solid Rep");
+		}
 	}
 	for(i = 0;i < senateSolidR.length;i++) {
 		console.log("Set as Solid R: " + senateSolidR[i]);
-		document.getElementById(senateSolidR[i]).setAttribute("class","Solid Rep");
+		if(document.getElementById(senateSolidR[i]) != undefined) {
+			document.getElementById(senateSolidR[i]).setAttribute("class","Solid Rep");
+		}
 	}
 	
 	for(i = 0;i < houseLikelyR.length;i++) {
 		console.log("Set as Likely R: " + houseLikelyR[i]);
-		document.getElementById(houseLikelyR[i]).setAttribute("class","Likely Rep");
+		if(document.getElementById(houseLikelyR[i]) != undefined) {
+			document.getElementById(houseLikelyR[i]).setAttribute("class","Likely Rep");
+		}
 	}
 	for(i = 0;i < senateLikelyR.length;i++) {
 		console.log("Set as Likely R: " + senateLikelyR[i]);
-		document.getElementById(senateLikelyR[i]).setAttribute("class","Likely Rep");
+		if(document.getElementById(senateLikelyR[i]) != undefined) {
+			document.getElementById(senateLikelyR[i]).setAttribute("class","Likely Rep");
+		}
 	}
 	
 	for(i = 0;i < houseLeanR.length;i++) {
 		console.log("Set as Lean R: " + houseLeanR[i]);
-		document.getElementById(houseLeanR[i]).setAttribute("class","Lean Rep");
+		if(document.getElementById(houseLeanR[i]) != undefined) {
+			document.getElementById(houseLeanR[i]).setAttribute("class","Lean Rep");
+		}
 	}
 	for(i = 0;i < senateLeanR.length;i++) {
 		console.log("Set as Lean R: " + senateLeanR[i]);
-		document.getElementById(senateLeanR[i]).setAttribute("class","Lean Rep");
+		if(document.getElementById(senateLeanR[i]) != undefined) {
+			document.getElementById(senateLeanR[i]).setAttribute("class","Lean Rep");
+		}
 	}
 	
 	for(i = 0;i < houseTossup.length;i++) {
 		console.log("Set as Tossup: " + houseTossup[i]);
-		document.getElementById(houseTossup[i]).setAttribute("class","Tossup");
+		if(document.getElementById(houseTossup[i]) != undefined) { 
+			document.getElementById(houseTossup[i]).setAttribute("class","Tossup");
+		}
 	}
 	for(i = 0;i < senateTossup.length;i++) {
 		console.log("Set as Tossup: " + senateTossup[i]);
-		document.getElementById(senateTossup[i]).setAttribute("class","Tossup");
+		if(document.getElementById(senateTossup[i]) != undefined) {
+			document.getElementById(senateTossup[i]).setAttribute("class","Tossup");
+		}
 	}
 	
 	for(i = 0;i < houseLeanD.length;i++) {
 		console.log("Set as Lean D: " + houseLeanD[i]);
-		document.getElementById(houseLeanD[i]).setAttribute("class","Lean Dem");
+		if(document.getElementById(houseLeanD[i]) != undefined) {
+			document.getElementById(houseLeanD[i]).setAttribute("class","Lean Dem");
+		}
 	}
 	for(i = 0;i < senateLeanD.length;i++) {
 		console.log("Set as Lean D: " + senateLeanD[i]);
-		document.getElementById(senateLeanD[i]).setAttribute("class","Lean Dem");
+		if(document.getElementById(senateLeanD[i]) != undefined) {
+			document.getElementById(senateLeanD[i]).setAttribute("class","Lean Dem");
+		}
 	}
 	
 	for(i = 0;i < houseLikelyD.length;i++) {
 		console.log("Set as Likely D: " + houseLikelyD[i]);
-		document.getElementById(houseLikelyD[i]).setAttribute("class","Likely Dem");
+		if(document.getElementById(houseLikelyD[i]) != undefined) {
+			document.getElementById(houseLikelyD[i]).setAttribute("class","Likely Dem");
+		}
 	}
 	for(i = 0;i < senateLikelyD.length;i++) {
 		console.log("Set as Likely D: " + senateLikelyD[i]);
-		document.getElementById(senateLikelyD[i]).setAttribute("class","Likely Dem");
+		if(document.getElementById(senateLikelyD[i]) != undefined) {
+			document.getElementById(senateLikelyD[i]).setAttribute("class","Likely Dem");
+		}
 	}
 	
 	for(i = 0;i < houseSolidD.length;i++) {
 		console.log("Set as Solid D: " + houseSolidD[i]);
-		document.getElementById(houseSolidD[i]).setAttribute("class","Solid Dem");
+		if(document.getElementById(houseSolidD[i]) != undefined) {
+			document.getElementById(houseSolidD[i]).setAttribute("class","Solid Dem");
+		}
 	}
 	for(i = 0;i < senateSolidD.length;i++) {
 		console.log("Set as Solid D: " + senateSolidD[i]);
-		document.getElementById(senateSolidD[i]).setAttribute("class","Solid Dem");
+		if(document.getElementById(senateSolidD[i]) != undefined) {
+			document.getElementById(senateSolidD[i]).setAttribute("class","Solid Dem");
+		}
 	}
 	
 	document.getElementById("power_graph_solid_rep_house").innerHTML  = houseSolidR.length;
